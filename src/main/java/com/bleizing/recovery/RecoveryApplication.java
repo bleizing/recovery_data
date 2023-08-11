fc1a07fddcd09461a4237e931c4e8c1f675ea08f
Bleizing <dimasz_97@yahoo.com>
2023-08-11 10:49:41 +0700

    Setting POM

--- src/main/java/com/bleizing/recovery/RecoveryApplication.java
+++ src/main/java/com/bleizing/recovery/RecoveryApplication.java
@@ -2,6 +2,7 @@ package com.bleizing.recovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class RecoveryApplication {
